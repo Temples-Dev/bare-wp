f5b2040ef8e9279ff28c0cbb2f77dfb1a80dee53
Temples-Dev <templelomotey@gmail.com>
2026-03-16 12:54:56 +0000

    fix: disable SECURE_AUTH_COOKIE for local development

---
+++ public/wp-core/wp-config.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * The base configuration for WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', getenv('DB_NAME') ?: 'bare_wp_db');
+
+/** Database username */
+define('DB_USER', getenv('DB_USER') ?: 'bare_wp_user');
+
+/** Database password */
+define('DB_PASSWORD', getenv('DB_PASSWORD') ?: 'bare_wp_pass');
+
+/** Database hostname */
+define('DB_HOST', getenv('DB_HOST') ?: 'db');
+
+/** Database charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The database collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ */
+define('AUTH_KEY', 'put your unique phrase here');
+define('SECURE_AUTH_KEY', 'put your unique phrase here');
+define('LOGGED_IN_KEY', 'put your unique phrase here');
+define('NONCE_KEY', 'put your unique phrase here');
+define('AUTH_SALT', 'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT', 'put your unique phrase here');
+define('NONCE_SALT', 'put your unique phrase here');
+
+/**#@-*/
+define('COOKIEPATH', '/');
+define('SITECOOKIEPATH', '/');
+define('ADMIN_COOKIE_PATH', '/');
+define('PLUGINS_COOKIE_PATH', '/');
+
+/**
+ * Define Site and Home URLs explicitly for the headless setup.
+ */
+define( 'WP_HOME', 'http://localhost:8080' );
+define( 'WP_SITEURL', 'http://localhost:8080/wp-core' );
+define( 'SECURE_AUTH_COOKIE', false );
+
+/**
+ * WordPress database table prefix.
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ */
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if (!defined('ABSPATH')) {
+	define('ABSPATH', __DIR__ . '/');
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
